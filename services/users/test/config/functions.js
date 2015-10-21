f9375ee6538f8a6641ae5da8b8fe8106ef305673
demiurgosoft <demiurgosoft@hotmail.com>
2015-10-21 02:03:50 +0200

    minor changes, added coverage to test

--- services/users/test/config/functions.js
+++ services/users/test/config/functions.js
@@ -1,23 +1,23 @@
-var mongoose=require('mongoose');
+var mongoose = require('mongoose');
 
 var User = require('../../app/models/user.js');
 var Config = require('./server.js');
 
 
 var express = require('express');
-var bodyParser=require('body-parser');
+var bodyParser = require('body-parser');
 
 var server;
 //auxiliary functions for testing
-module.exports={
-    clearUsers: function(done) {
-    	User.remove({}, function(err, res) {
-    		if (err) done(err);
-    		else done();
-    	});
-    },
-    connectDB: function(done){
-        mongoose.connect(Config.dbUrl);
+module.exports = {
+	clearUsers: function(done) {
+		User.remove({}, function(err, res) {
+			if (err) done(err);
+			else done();
+		});
+	},
+	connectDB: function(done) {
+		mongoose.connect(Config.dbUrl);
 		var db = mongoose.connection;
 		/*db.on('error', function(err) {
 			done(err);
@@ -25,21 +25,21 @@ module.exports={
 		db.once('open', function() {
 			done();
 		});
-        return db;
-    },
-    setupServer: function(done){
-        var app=express();
-        app.use(bodyParser.json()); // get information from body
-        app.use(bodyParser.urlencoded({
-        	extended: true
-        }));
-        require('../../app/routes.js')(app);
-        server=app.listen(Config.serverPort, function() {
-            done();
-    	});
-        return app;
-    },
-    closeServer: function(){
-        if(server) server.close();
-    } 
+		return db;
+	},
+	setupServer: function(done) {
+		var app = express();
+		app.use(bodyParser.json()); // get information from body
+		app.use(bodyParser.urlencoded({
+			extended: true
+		}));
+		require('../../app/routes.js')(app);
+		server = app.listen(Config.serverPort, function() {
+			done();
+		});
+		return app;
+	},
+	closeServer: function() {
+		if (server) server.close();
+	}
 }
