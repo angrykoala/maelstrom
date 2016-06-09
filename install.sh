2f01ef67bcdbe0aad20f32682b4d64e74f7eb78d
demiurgosoft <demiurgosoft@hotmail.com>
2016-06-09 08:14:16 +0200

    0.5

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-rm -rf maelstrom-users maelstrom-world;
+rm -rf maelstrom-users maelstrom-world maelstrom-web maelstrom-bot;
 git clone https://github.com/demiurgosoft/maelstrom-users;
 git clone https://github.com/demiurgosoft/maelstrom-world;
+git clone https://github.com/demiurgosoft/maelstrom-web;
+git clone https://github.com/demiurgosoft/maelstrom-bot;
 
 cd maelstrom-users;
 npm install;
 cd ../maelstrom-world;
+npm install;
+cd ../maelstrom-bot;
+npm install;
+cd ../maelstrom-web;
 npm install;
 cd ..;
